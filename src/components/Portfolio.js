38cd85176bca18b504038cb7dce1c16559b6ccb3
AE SChan <abigailsinchan@gmail.com>
2023-08-15 22:10:59 -0400

    import and added images link. started basic css for profile.css

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,66 +1,86 @@
 import React from 'react';
 import '../css/Portfolio.css';
+import rhythmReviewImg from '../images/rhythmReview.png';
+import howRuFeelingImg from '../images/howRUFeeling.png';
+import noteTakerImg from '../images/noteTaker.png';
+import weatherDashboardImg from '../images/weatherApp.png';
+import workDaySchedularImg from '../images/workdaySchedular.png';
+import quizMasterImg from '../images/quizMaster.png';
 
 function Portfolio() {
   const projects = [
     {
       title: 'Rhythm Review',
-      imageUrl: 'project1.jpg',
+      imageUrl: rhythmReviewImg, 
       deployedLink: 'https://agile-springs-19422-e0a9573c7a30.herokuapp.com/',
       githubLink: 'https://github.com/delantetr/literate-guacamole',
+      languages: ['React', 'Node.js', 'Express', 'MongoDB'],
     },
     {
-        title: 'How R U Feeling',
-        imageUrl: 'project1.jpg',
-        deployedLink: 'https://abisinchan.github.io/verbose-potato/',
-        githubLink: 'https://github.com/abisinchan/verbose-potato',
-      },
-      {
-        title: 'Note Taker',
-        imageUrl: 'project1.jpg',
-        deployedLink: 'https://aqueous-taiga-53591-4655247aa37e.herokuapp.com/notes',
-        githubLink: 'https://github.com/abisinchan/crispy-octo-robot',
-      },
-      {
-        title: 'Weather DashBoard',
-        imageUrl: 'project1.jpg',
-        deployedLink: 'https://abisinchan.github.io/animated-succotash/',
-        githubLink: 'https://github.com/abisinchan/animated-succotash',
-      },
-      {
-        title: 'Work Day Schedular',
-        imageUrl: 'project1.jpg',
-        deployedLink: 'https://abisinchan.github.io/symmetrical-meme/',
-        githubLink: 'https://github.com/abisinchan/symmetrical-meme',
-      },
-      {
-        title: 'Quiz Master',
-        imageUrl: 'project1.jpg',
-        deployedLink: 'https://abisinchan.github.io/ideal-barnacle/',
-        githubLink: 'https://github.com/abisinchan/ideal-barnacle',
-      }
-  ];
-
-  return (
+      title: 'How R U Feeling',
+      imageUrl: howRuFeelingImg, 
+      deployedLink: 'https://abisinchan.github.io/verbose-potato/',
+      githubLink: 'https://github.com/abisinchan/verbose-potato',
+      languages: ['React', 'Node.js', 'Express', 'MongoDB'],
+    },
+    {
+      title: 'Note Taker',
+      imageUrl: noteTakerImg,
+      deployedLink: 'https://aqueous-taiga-53591-4655247aa37e.herokuapp.com/notes',
+      githubLink: 'https://github.com/abisinchan/crispy-octo-robot',
+      languages: ['React', 'Node.js', 'Express', 'MongoDB'],
+    },
+    {
+      title: 'Weather DashBoard',
+      imageUrl: weatherDashboardImg,
+      deployedLink: 'https://abisinchan.github.io/animated-succotash/',
+      githubLink: 'https://github.com/abisinchan/animated-succotash',
+      languages: ['React', 'Node.js', 'Express', 'MongoDB'],
+    },
+    {
+      title: 'Work Day Schedular',
+      imageUrl: workDaySchedularImg,
+      deployedLink: 'https://abisinchan.github.io/symmetrical-meme/',
+      githubLink: 'https://github.com/abisinchan/symmetrical-meme',
+      languages: ['React', 'Node.js', 'Express', 'MongoDB'],
+    },
+    {
+      title: 'Quiz Master',
+      imageUrl: quizMasterImg,
+      deployedLink: 'https://abisinchan.github.io/ideal-barnacle/',
+      githubLink: 'https://github.com/abisinchan/ideal-barnacle',
+      languages: ['React', 'Node.js', 'Express', 'MongoDB'],
+    },
+    ];
+return (
     <section className="portfolio-section">
-    <div className="portfolio-items">
-      {projects.map((project, index) => (
-        <div key={index} className="portfolio-item">
-          <img src={project.imageUrl} alt={project.title} />
-          <h3>{project.title}</h3>
-          <div className="links">
+      <div className="portfolio-items">
+        {projects.map((project, index) => (
+          <div key={index} className="portfolio-item">
+            <h3>{project.title}</h3>
             <a href={project.deployedLink} target="_blank" rel="noopener noreferrer">
-              Demo
+              <div className="portfolio-item-hover">
+                
+                <img src={project.imageUrl} alt={project.title} />
+                <div className="languages">
+                  {project.languages.map((language, langIndex) => (
+                    <span key={langIndex}>{language}</span>
+                  ))}
+                </div>
+              </div>
             </a>
-            <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
-              Github Repo
-            </a>
+            <div className="links">
+              <a href={project.deployedLink} target="_blank" rel="noopener noreferrer">
+                Deployed Link
+              </a>
+              <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+                Github Repo Link
+              </a>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  </section>
-);
+        ))}
+      </div>
+    </section>
+  );
 }
-
 export default Portfolio;
